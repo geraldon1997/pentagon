2d01572e5005e16e1e9b3215be153dec925939ba
geraldon1997 <geraldon1997@gmail.com>
2020-07-22 07:30:18 -0400

    finished writing the admin model

--- logisticexpress/index.php
+++ logisticexpress/index.php
@@ -62,62 +62,7 @@
     <!-- our info End -->
     
     
-    <!--? Testimonial Start -->
-    <div class="testimonial-area testimonial-padding section-bg" data-background="assets/img/gallery/section_bg04.jpg">
-        <div class="container">
-            <div class="row justify-content-between">
-                <div class="col-xl-12 col-lg-12">
-                    <!-- Section Tittle -->
-                    <div class="section-tittle section-tittle2 mb-25">
-                        <span>Clients Testimonials</span>
-                        <h2>What Our Clients Say!</h2>
-                    </div> 
-                    <div class="h1-testimonial-active mb-70">
-                        <!-- Single Testimonial -->
-                        <div class="single-testimonial ">
-                            <!-- Testimonial Content -->
-                            <div class="testimonial-caption ">
-                                <div class="testimonial-top-cap">
-                                    <p>Srem ipsum adolor dfsit amet, consectetur adipiscing elit, sed dox beiusmod tempor incci didunt ut labore et dolore magna aliqua. Quis cipsucm suspendisse ultrices gravida. Risus commodo vivercra maecenas accumsan lac.</p>
-                                </div>
-                                <!-- founder -->
-                                <div class="testimonial-founder d-flex align-items-center">
-                                    <div class="founder-img">
-                                        <img src="assets/img/gallery/Homepage_testi.png" alt="">
-                                    </div>
-                                    <div class="founder-text">
-                                        <span>Jhaon smith</span>
-                                        <p>Creative designer</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Single Testimonial -->
-                        <div class="single-testimonial ">
-                            <!-- Testimonial Content -->
-                            <div class="testimonial-caption ">
-                                <div class="testimonial-top-cap">
-                                    <p>Srem ipsum adolor dfsit amet, consectetur adipiscing elit, sed dox beiusmod tempor incci didunt ut labore et dolore magna aliqua. Quis cipsucm suspendisse ultrices gravida. Risus commodo vivercra maecenas accumsan lac.</p>
-                                </div>
-                                <!-- founder -->
-                                <div class="testimonial-founder d-flex align-items-center">
-                                    <div class="founder-img">
-                                        <img src="assets/img/gallery/Homepage_testi.png" alt="">
-                                    </div>
-                                    <div class="founder-text">
-                                        <span>Jhaon smith</span>
-                                        <p>Creative designer</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-    </div>
-    <!-- Testimonial End -->
+    
     
 </main>
 
