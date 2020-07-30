7a88c8cc89add6bb222222abf0dbc201963666b6
geraldon1997 <geraldon199@gmail.com>
2020-07-30 05:40:45 -0400

    changed map view

--- logisticexpress/track.php
+++ logisticexpress/track.php
@@ -56,29 +56,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 
                 <div class='row'>
                     <div class='col-12 text-center'>
-                        <div class='mapouter'>
-                            <div class='gmap_canvas'>
-                                <iframe id='gmap_canvas' src='https://maps.google.com/maps?q=$shipment->location&t=k&z=13&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'></iframe>
-                            </div>
-                            <style>
-                            iframe{
-                                width:100%;
-                                height:500px;
-                            }
-                            .mapouter{
-                                position:relative;
-                                text-align:right;
-                                height:500px;
-                                width:100%;
-                            }
-                            .gmap_canvas {
-                                overflow:hidden;
-                                background:none!important;
-                                height:500px;
-                                width:100%;
-                            }
-                            </style>
-                        </div>
+                        
+                    <div class='mapouter'>
+                    <div class='gmap_canvas'>
+                    <iframe 
+                         
+                        id='gmap_canvas' 
+                        src='https://maps.google.com/maps?q=$shipment->location&t=&z=13&ie=UTF8&iwloc=&output=embed' 
+                        frameborder='0' 
+                        scrolling='no' 
+                        marginheight='0' 
+                        marginwidth='0'>
+                    </iframe>
+                    </div>
+                    <style>
+                        .mapouter{
+                            position:relative;
+                            // text-align:right;
+                            height:500px;
+                            width:100%;
+                        }
+                        .gmap_canvas {
+                            overflow:hidden;
+                            background:none!important;
+                            height:500px;
+                            width:100%;
+                        }
+                        iframe{
+                            width: 100%;
+                            height: 500px;
+                        }
+                    </style>
+                    </div>
+                        
                     </div>
                 </div>
 
